f47142fe71de31ecc6e037af099972bcbe449825
vova-miretskyi <vova.miretskyi@gmail.com>
2026-05-05 16:17:56 +0300

    added config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"log/slog"
+	"os"
 
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+	"github.com/volodymyr-miretskyi/go-nutrition/internal/app/config"
+)
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	ctx := context.Background()
+
+	cfg := config.LoadConfig()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	conn, dbConnectionErr := pgx.Connect(ctx, cfg.DB.Dsn)
+
+	if dbConnectionErr != nil {
+		slog.Error("database is failed to connect", "error", dbConnectionErr)
+		os.Exit(1)
+	}
+
+	defer conn.Close(ctx)
 }
---
+++ internal/app/config/config.go
@@ -0,0 +1,84 @@
+package config
+
+import "os"
+
+type Config struct {
+	APP_ENV string
+	Port    string
+	DB      *db
+	JWT     *jwt
+	S3      *s3
+	OpenAi  *openAi
+}
+
+type db struct {
+	Dsn string
+}
+
+type jwt struct {
+	Secret string
+}
+
+type s3 struct {
+	Bucket string
+	Region string
+}
+
+type openAi struct {
+	ApiKey string
+}
+
+func initDb() *db {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	name := os.Getenv("DB_NAME")
+	password := os.Getenv("DB_PASSWORD")
+
+	return &db{Dsn: "host=" + host + " port=" + port + " user=" + user + " dbname=" + name + " password=" + password + " sslmode=disable"}
+}
+
+func initS3() *s3 {
+	bucket := os.Getenv("S3_BUCKET")
+	region := os.Getenv("S3_REGION")
+
+	return &s3{
+		Bucket: bucket,
+		Region: region,
+	}
+}
+
+func initJwt() *jwt {
+	secret := os.Getenv("JWT_SECRET")
+
+	return &jwt{
+		Secret: secret,
+	}
+}
+
+func initOpenAi() *openAi {
+	apiKey := os.Getenv("OPENAI_API_KEY")
+
+	return &openAi{
+		ApiKey: apiKey,
+	}
+}
+
+func LoadConfig() *Config {
+	appEnv := os.Getenv("APP_ENV")
+	port := os.Getenv("PORT")
+
+	db := initDb()
+	s3 := initS3()
+	jwt := initJwt()
+	openAI := initOpenAi()
+
+	return &Config{
+		APP_ENV: appEnv,
+		Port:    port,
+		DB:      db,
+		S3:      s3,
+		JWT:     jwt,
+		OpenAi:  openAI,
+	}
+}
